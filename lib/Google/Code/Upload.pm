9cff0ef3f40b4b54cb2c80de74fe7de01c6673ad
fayland <fayland@gmail.com>
2010-02-05 11:09:27 +0800

    from fayland repos

---
+++ lib/Google/Code/Upload.pm
@@ -0,0 +1,129 @@
+package Google::Code::Upload;
+
+use warnings;
+use strict;
+use File::Spec ();
+use File::Basename ();
+use List::MoreUtils qw/natatime/;
+use MIME::Base64;
+use LWP::UserAgent;
+use HTTP::Headers;
+use HTTP::Request;
+
+use base 'Exporter';
+use vars qw/@EXPORT_OK/;
+@EXPORT_OK = qw/ upload /;
+
+our $VERSION = '0.04';
+our $AUTHORITY = 'cpan:FAYLAND';
+
+sub upload {
+    my ( $file, $project_name, $username, $password, $summary, $labels ) = @_;
+    
+    $labels ||= [];
+    if ( $username =~ /^(.*?)\@gmail\.com$/ ) {
+        $username = $1;
+    }
+
+    my @form_fields = (
+        summary => $summary,
+    );
+    push @form_fields, ( label => $_ ) foreach (@$labels);
+    
+    my ( $content_type, $body ) = encode_upload_request(\@form_fields, $file);
+    
+    my $upload_uri  = "https://$project_name.googlecode.com/files";
+    my $auth_token  = encode_base64("$username:$password", '');
+
+    my $header = HTTP::Headers->new;
+    $header->header('Authorization' => "Basic $auth_token");
+    $header->header('User-Agent' => 'Googlecode.com uploader v0.9.4');
+    $header->header('Content-Type' => $content_type);
+  
+    my $ua = LWP::UserAgent->new(
+        agent => 'Googlecode.com uploader v0.9.4',
+    );
+    my $request = HTTP::Request->new(POST =>$upload_uri, $header, $body);
+    my $response = $ua->request($request);
+
+    if ($response->code == 201) {
+        return ( $response->code, $response->status_line, $response->header('Location') );
+    } else {
+        return ( $response->code, $response->status_line, undef );
+    }
+}
+
+sub encode_upload_request {
+    my ($form_fields, $file) = @_;
+    
+    my $BOUNDARY = '----------Googlecode_boundary_reindeer_flotilla';
+    my $CRLF = "\r\n";
+
+    my @body;
+    
+    my $it = natatime 2, @$form_fields;
+    while (my ( $key, $val ) = $it->()) {
+        push @body, (
+            "--$BOUNDARY",
+            qq~Content-Disposition: form-data; name="$key"~,
+            '',
+            $val
+        );
+    }
+    
+    my $filename = File::Basename::basename($file);
+    local $/;
+    open(my $fh, '<', $file) or die $!;
+    my $content = <$fh>;
+    close($fh);
+    
+    push @body, (
+        "--$BOUNDARY",
+        qq~Content-Disposition: form-data; name="filename"; filename="$filename"~,
+        # The upload server determines the mime-type, no need to set it.
+        'Content-Type: application/octet-stream',
+        '',
+        $content,
+    );
+
+    # Finalize the form body
+    push @body, ("--$BOUNDARY--", '');
+
+    return ("multipart/form-data; boundary=$BOUNDARY", join( $CRLF, @body ) );
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Google::Code::Upload - uploading files to a Google Code project.
+
+=head1 SYNOPSIS
+
+    use Google::Code::Upload qw/upload/;
+
+    upload( $file, $project_name, $username, $password, $summary, $labels );
+
+=head1 DESCRIPTION
+
+It's an incomplete Perl port of L<http://support.googlecode.com/svn/trunk/scripts/googlecode_upload.py>
+
+basically you need L<googlecode_upload> script instead.
+
+=head1 METHODS
+
+=head2 upload
+
+    upload( $file, $project_name, $username, $password, $summary, $labels );
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
---
+++ script/googlecode_upload.pl
@@ -0,0 +1,131 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+our $VERSION = '0.04';
+our $AUTHORITY = 'cpan:FAYLAND';
+
+use Getopt::Long;
+use Pod::Usage;
+use Term::ReadKey;
+use Google::Code::Upload qw/upload/;
+
+my %params;
+
+GetOptions(
+	\%params,
+	"help|?",
+	"s|summary=s",
+	"n|project=s",
+	"u|user=s",
+	"p|pass=s",
+	"l|labels=s",
+);
+
+my $file = pop @ARGV;
+unless ($file) { pod2usage(1); }
+-e $file or die "$file is not found\n";
+
+unless ( exists $params{n} ) {
+	print "Please enter your project name: ";
+	while ( $params{n} = ReadLine(0) ) {
+		chomp($params{n});
+		last if $params{n};
+	}
+}
+unless ( exists $params{u} ) {
+	print "Please enter your googlecode.com username: ";
+	while ( $params{u} = ReadLine(0) ) {
+		chomp($params{u});
+		last if $params{u};
+	}
+}
+unless ( exists $params{p} ) {
+    ReadMode('noecho');
+	print "** Note that this is NOT your Gmail account password! **\n",
+		"It is the password you use to access Subversion repositories,\n",
+		"and can be found here: http://code.google.com/hosting/settings\n",
+		"your password: ";
+	while ( $params{p} = ReadLine(0) ) {
+		chomp($params{p});
+		last if $params{p};
+	}
+	ReadMode 'normal';
+}
+unless ( exists $params{s} ) {
+	print "\nPlease enter your file summary: ";
+	while ( $params{s} = ReadLine(0) ) {
+		chomp($params{s});
+		last if $params{s};
+	}
+}
+
+my @labels;
+if ( exists $params{l} ) {
+	@labels = split(/\,\s*/, $params{l} );
+} else {
+    print "Please enter your file labels (eg: 'Featured, Type-Source, OpSys-All'): ";
+	while ( my $labels = ReadLine(0) ) {
+		chomp($labels);
+		@labels = split(/\,\s*/, $labels);
+		last;
+    }
+}
+
+my ( $status, $reason, $url ) = 
+	upload( $file, $params{n}, $params{u}, $params{p}, $params{s}, \@labels );
+
+if ( $url ) {
+	print "The file was uploaded successfully.\nURL: $url\n";
+} else {
+	print "An error occurred. Your file was not uploaded.\nGoogle Code upload server said: $reason ($status)\n";
+}
+
+1;
+__END__
+
+=head1 NAME
+
+googlecode_upload - script uploading files to a Google Code project.
+
+=head1 SYNOPSIS
+
+    googlecode_upload.pl [options] FILE
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-?>, B<--help>
+
+=item B<s|summary>
+
+Short description of the file
+
+=item B<n|project>
+
+Google Code project name
+
+=item B<u|user>
+
+Your Google Code Subversion username
+
+=item B<p|pass=s>
+
+Your Google Code Subversion password
+
+=item B<l|labels>
+
+An optional list of labels to attach to the file
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
